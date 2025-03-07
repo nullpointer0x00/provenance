60f5bc1282c652db9a818846245b53cbc7c4d01b
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-03-07 07:13:45 -0700

    add writing of file

--- x/metadata/keeper/migrations_v4.go
+++ x/metadata/keeper/migrations_v4.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/address"
 	"github.com/cosmos/gogoproto/proto"
 
+	internalsdk "github.com/provenance-io/provenance/internal/sdk"
 	markertypes "github.com/provenance-io/provenance/x/marker/types"
 	"github.com/provenance-io/provenance/x/metadata/types"
 )
@@ -22,9 +23,9 @@ func (m Migrator) Migrate3To4(ctx sdk.Context) error {
 	// So we'll just throw all of them away by swapping in a different event manager here.
 	// But the testnet migration already ran using v1.20.0-rc2 which included all of the events in the block result.
 	// So, to keep v1.20.0-rc2 and v1.20.0 state compatible, we only throw out the events when not on a testnet.
-	// if sdk.GetConfig().GetBech32AccountAddrPrefix() != "tp" {
-	// 	ctx = ctx.WithEventManager(internalsdk.NewNoOpEventManager())
-	// }
+	if sdk.GetConfig().GetBech32AccountAddrPrefix() != "tp" {
+		ctx = ctx.WithEventManager(internalsdk.NewNoOpEventManager())
+	}
 	logger := m.keeper.Logger(ctx)
 	logger.Info("Starting migration of x/metadata from 3 to 4.")
 	if err := migrateValueOwners(ctx, newKeeper3To4(m.keeper)); err != nil {
